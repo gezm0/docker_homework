1a44ed4ba4dc4fb9e09cff4c24664d98cf99869a
gezm0 <eremin@ryazan.net.ru>
2022-03-06 18:56:59 +0300

    docker lesson 2 homework

--- docker_run_all.sh
+++ docker_run_all.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 docker run -d -p 3000:3000 --name frontend_app --net app_net --ip=10.0.3.100 frontend
-docker run -i --name lib_catalog --net app_net --ip=10.0.3.101 lib_catalog
+docker run -d -p 8000:8000 --name backend_app --net app_net --ip=10.0.3.101 backend
 
-# db with env variables usage
-#docker run -d -p 5432:5432 --name postgresql_app --net app_net --ip=10.0.3.102 postgres:alpine3.14
+# . docker_db_env.sh
+# docker run -d -p 5432:5432 --name postgresql_app --net app_net --ip=10.0.3.102 postgres:alpine3.14
 
-docker run -d -p 5432:5432 --name postgresql_app --net app_net --ip=10.0.3.102 -e POSTGRES_USER=test -e POSTGRES_PASSWORD=test -e POSTGRES_DB=test postgres:alpine3.14
+docker run -d -p 5432:5432 --name postgresql_app --net app_net --ip=10.0.3.102 -e POSTGRES_USER=django -e POSTGRES_PASSWORD=django -e POSTGRES_NAME=django postgres:alpine3.14
