975289bcb2bcdc3ed2e4ba7582c775816ec4b8a0
gezm0 <eremin@ryazan.net.ru>
2022-03-07 20:50:46 +0300

    added CORS_ALLOWED_ORIGINS

--- lib_catalog/lib_catalog/settings.py
+++ lib_catalog/lib_catalog/settings.py
@@ -139,4 +139,8 @@ CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
+
 FIXTURES_DIRS = os.path.join(BASE_DIR, 'catalog/fixtures')
